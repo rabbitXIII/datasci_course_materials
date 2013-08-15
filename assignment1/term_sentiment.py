50153fa22435cb1f353d5f6df9e8d753dc7d8c77
Rohit Gopal <rohit@collegerail.com>
2013-08-15 19:45:53 +0000

    Fixes #1 takes the average of the sentiments for the new terms

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -27,26 +27,30 @@ def sentiment_score(text, scores):
 		if scores.has_key(word): score += scores[word]
 	return score
 
-def update_scores_for_new_words(text, scores, sentiment):
-	for word in text.replace('.','').replace(',','').rsplit(' '):
-		if not word in scores:
-			scores[word] = sentiment
-	return scores
+def update_scores_for_new_words(text, scores, new_scores, sentiment):
+	for term in text.replace('\n','').replace('.','').replace(',','').rsplit(' '):
+		if not term in scores and term != "":
+			if not term in new_scores:
+				new_scores[term] = []
+			new_scores[term].append(sentiment)
+	return new_scores
 
 def main():
 	sent_file = open(sys.argv[1])
 	tweet_file = open(sys.argv[2])
 	scores = parse_sentiments(sent_file.readlines())
 	tweets = dict_from_json_strings(tweet_file.readlines())
+	new_scores = {}
 	for tweet in tweets:
 		try:
 			score_for_tweet = sentiment_score(tweet['text'], scores)
-			scores = update_scores_for_new_words(tweet['text'],scores,score_for_tweet)	
+			new_scores = update_scores_for_new_words(tweet['text'],scores,new_scores,score_for_tweet)	
 		except(KeyError):
 			pass
-	for word in scores:
+	for term,score_list in new_scores.iteritems():
+		average_score = float(sum(score_list))/float(len(score_list))
 		try:
-			print '{} {}'.format(word,scores[word])
+			print '{} {}'.format(term,average_score)
 		except(UnicodeEncodeError):
 			pass
 

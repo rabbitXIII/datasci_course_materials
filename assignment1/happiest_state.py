66b9df70fde683a5e7b3d26fd1948ac2b42be14c
Rohit Gopal <rohit@collegerail.com>
2013-08-16 19:55:57 +0000

    #3 - prints out sentiments for locations. needs to only include places in the US

--- assignment1/happiest_state.py
+++ assignment1/happiest_state.py
@@ -15,17 +15,36 @@ def parse_sentiments(sentiments):
 		scores[term] = float(score)
 	return scores
 
+def sentiment_score(text, scores):
+	score = 0
+	for word in text.split():
+		if scores.has_key(word): score += scores[word]
+	return score
 
 def main():
 	sent_file = open(sys.argv[1])
 	tweet_file = open(sys.argv[2])
 	scores = parse_sentiments(sent_file.readlines())
 	tweets = dict_from_json_strings(tweet_file.readlines())
+	location_sentiment_collection = {}
 	for tweet in tweets:
+		location = None
 		try:
-			print tweet['coordinates']
-			print tweet['place']
-			print tweet['user']['location']
+			if tweet['coordinates'] is not None:
+				location = tweet['coordinates']
+				# this needs to be looked up to find a state
+			if tweet['place'] is not None:
+				location = tweet['place']['fullname']
+			elif tweet['user']['location'] is not None:
+				location = tweet['user']['location']
+
+			if location is not None and location != "":
+				print location
+				if location not in location_sentiment_collection:
+					location_sentiment_collection[location] = []
+				location_sentiment_collection[location].append(sentiment_score(tweet['text'],scores))
+				print location_sentiment_collection[location]
+
 		except(KeyError):
 			pass
 

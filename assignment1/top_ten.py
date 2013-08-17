13156d5a9363ebc398464398bde4bdcb107351ed
Rohit Gopal <rohit@collegerail.com>
2013-08-17 22:59:19 +0000

    #4 - prints out the top ten hashtags

--- assignment1/top_ten.py
+++ assignment1/top_ten.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import operator
 
 def dict_from_json_strings(json_list):
 	tweets = []
@@ -11,12 +12,22 @@ def dict_from_json_strings(json_list):
 def main():
 	tweet_file = open(sys.argv[1])
 	tweets = dict_from_json_strings(tweet_file.readlines())
+	hashtag_counts = {}
 	for tweet in tweets:
 		try:
-			print tweet['entities']['hashtags']
+			hashtags = tweet['entities']['hashtags']
+			for hashtag in hashtags:
+				if hashtag['text'] not in hashtag_counts:	
+					hashtag_counts[hashtag['text']] = 0
+				hashtag_counts[hashtag['text']] += 1
 		except(KeyError):
 			pass
+	sorted_hashtag_list = sorted(hashtag_counts.iteritems(), key=operator.itemgetter(1))
+	# sorts the counts in increasing order
+	sorted_hashtag_list.reverse()
 
+	for i in range(10):
+		print "{} {}".format(sorted_hashtag_list[i][0], sorted_hashtag_list[i][1])
 
 if __name__ == '__main__':
 	main()

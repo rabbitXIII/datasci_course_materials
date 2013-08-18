ef44002b8d6f75381cb345a4573cfba2ade6e053
Rohit Gopal <rohit@collegerail.com>
2013-08-18 19:10:43 +0000

    #5 - subproblem C complete

--- assignment2/problem1.sql
+++ assignment2/problem1.sql
@@ -3,3 +3,10 @@ select count(*) from frequency where docid = '10398_txt_earn';
 
 -- subproblem b
 select count(*) from frequency where docid = '10398_txt_earn' and count=1;
+
+-- subproblem c
+select count(*) from 
+	( select * from frequency where docid = '10398_txt_earn' and count = 1 
+		UNION 
+		select * from frequency where docid = '925_txt_trade' and count = 1 );
+
